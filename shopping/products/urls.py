c4197c977c85d7f31d6257b5d4f626f229e1644e
BHARATH B N <bharathbn17498@gmail.com>
2023-02-14 11:00:57 +0530

    added product.html

--- shopping/products/urls.py
+++ shopping/products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import product
+from .views import product,product_list
 
 urlpatterns = [
     path('adddata/', product),
+    path('product/<int:id>/',product_list),
+    
 ]
--- shopping/products/views.py
+++ shopping/products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import product_db
 # Create your views here.
 import pandas as pd
@@ -9,3 +10,9 @@ def product(request, *args, **kwargs):
     items = product_db.objects.all()
     print(request.method)
     return render(request, 'products/home.html', {'items': items})
+
+def product_list(request, id,*args, **kwargs):
+    items = product_db.objects.get(id=id)
+    return HttpResponse(f'id: {items.id} , content: {items.content} , price: {items.price}')
+
+
